af73310b2f70d98ecbc445e0cd6eeda83da2edf7
dary410 <dary410@gmail.com>
2015-07-06 08:25:37 -0700

    test

--- _site/mail/contact_me.php
+++ _site/mail/contact_me.php
@@ -1,58 +1,26 @@
 <?php
-// use actual sendgrid username and password in this section
-$url = 'https://api.sendgrid.com/';
-$user = 'dary410'; // place SG username here
-$pass = 'ko3191501'; // place SG password here
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['phone']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
 
-// grabs HTML form's post data; if you customize the form.html parameters then you will need to reference their new new names here
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-// note the above parameters now referenced in the 'subject', 'html', and 'text' sections
-// make the to email be your own address or where ever you would like the contact form info sent
-$params = array(
-    'api_user'  => "$user",
-    'api_key'   => "$pass",
-    'to'        => "dary410@gmail.com", // set TO address to have the contact form's email content sent to
-    'subject'   => "Contact Form Submission", // Either give a subject for each submission, or set to $subject
-    'html'      => "<html><head><title> Contact Form</title><body>
-    Name: $name\n<br>
-    Email: $email\n<br>
-    Phone: $phone\n<br>
-    Message: $message <body></title></head></html>", // Set HTML here.  Will still need to make sure to reference post data names
-    'text'      => "
-    Name: $name\n
-    Email: $email\n
-    Phone: $phone\n
-    $message",
-    'from'      => "noreply@daryproduct.com", // set from address here, it can really be anything
-  );
-
-
-curl_setopt($curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-
-// Redirect to thank you page upon successfull completion, will want to build one if you don't alreday have one available
-header('Location: thanks.html'); // feel free to use whatever title you wish for thank you landing page, but will need to reference that file name in place of the present 'thanks.html'
-exit();
-
-// print everything out
-print_r($response);
-
+// Create the email and send the message
+$to = 'dary@daryproduct.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: dary@daryproduct.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
+mail($to,$email_subject,$email_body,$headers);
+return true;
 ?>
