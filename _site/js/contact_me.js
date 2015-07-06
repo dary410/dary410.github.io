58c51c2f70529c042dba2bc7c0a77876a926e859
dary410 <dary410@gmail.com>
2015-07-05 22:32:13 -0700

    potential change

--- _site/js/contact_me.js
+++ _site/js/contact_me.js
@@ -27,7 +27,9 @@ $(function() {
                     message: message
                 },
                 cache: false,
-                success: function() {
+                success: function(result) {
+                    $('#success').html(result);
+                    /*
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -36,11 +38,13 @@ $(function() {
                         .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
-
+                    */
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(result) {
+                    $('#success').html(result);
+                    /*
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -48,6 +52,7 @@ $(function() {
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
+                    */
                     $('#contactForm').trigger("reset");
                 },
             })
