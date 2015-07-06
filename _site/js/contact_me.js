7937208005f1e13233ec10910638096909ce5bc5
dary410 <dary410@gmail.com>
2015-07-05 22:44:48 -0700

    test

--- _site/js/contact_me.js
+++ _site/js/contact_me.js
@@ -20,16 +20,14 @@ $(function() {
             $.ajax({
                 url: "../mail/contact_me.php",
                 type: "POST",
-                data: $('#contactForm').serialize(),
+                data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
                 cache: false,
-                success: function(result) {
-                    $('#success').html(result);
-                    /*
+                success: function() {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -38,13 +36,11 @@ $(function() {
                         .append("<strong>Your message has been sent. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
-                    */
+
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function(result) {
-                    $('#success').html(result);
-                    /*
+                error: function() {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
@@ -52,7 +48,6 @@ $(function() {
                     $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
-                    */
                     $('#contactForm').trigger("reset");
                 },
             })
